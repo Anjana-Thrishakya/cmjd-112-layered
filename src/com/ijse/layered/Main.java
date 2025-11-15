49b06fe0b03bcd0c6859758578881ac09561c4d1
Anjana Thrishakya <anjanathrishakya95@gmail.com>
2025-11-15 13:06:21 +0530

    Item View and Controller

--- src/com/ijse/layered/Main.java
+++ src/com/ijse/layered/Main.java
@@ -4,6 +4,8 @@
  */
 package com.ijse.layered;
 
+import com.ijse.layered.view.ItemView;
+
 /**
  *
  * @author Anjana
@@ -14,7 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new ItemView().setVisible(true);
     }
     
 }
---
+++ src/com/ijse/layered/controller/ItemController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ijse.layered.controller;
+
+import com.ijse.layered.dto.ItemDto;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Anjana
+ */
+public class ItemController {
+    
+    public String saveItem(ItemDto itemDto) throws Exception{
+        return null;
+    }
+    
+    public ArrayList<ItemDto> getAll() throws Exception{
+        return null;
+    }
+    
+    public ItemDto searchItem(String code) throws Exception{
+        return null;
+    }
+    
+}
