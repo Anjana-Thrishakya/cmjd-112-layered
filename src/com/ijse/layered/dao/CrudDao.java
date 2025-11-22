674580038197795c12a496ece757a0932c8c91a6
Anjana Thrishakya <anjanathrishakya95@gmail.com>
2025-11-22 09:08:01 +0530

    CrudDao and CrudUtil

---
+++ src/com/ijse/layered/dao/CrudDao.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.ijse.layered.dao;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Anjana
+ */
+public interface CrudDao <T, ID> extends SuperDao{
+    public boolean save(T t) throws Exception;
+    public boolean update(T t) throws Exception;
+    public boolean delete(ID id) throws Exception;
+    public T search(ID id) throws Exception;
+    public ArrayList<T> getAll() throws Exception;
+}
